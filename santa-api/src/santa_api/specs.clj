35380876a70200ce7e1e4fe8b7a65054b4f77779
Flo <flal@melix.net>
2018-12-16 21:51:10 +0100

    specs

---
+++ santa-api/src/santa_api/specs.clj
@@ -0,0 +1,27 @@
+(ns santa-api.specs
+  (:require [clojure.spec.alpha :as s]))
+
+
+(defn recipient-valid? [name]
+  (re-matches #"[\w-_]+" name))
+
+;; -- wishlist meta-data
+(s/def ::recipient recipient-valid?)
+(s/def ::behavior (s/and pos-int? #(<= % 10)))
+(s/def ::wishlist (s/keys :req-un [::recipient ::behavior]))
+
+;; (s/explain-str ::wishlist {:behavior 4 :recipient "Florent")
+
+;; -- presents meta-data
+(s/def ::name string?)
+(s/def ::presents (s/coll-of ::name))
+(s/def ::wishlist-detailled (s/keys :req-un [::recipient
+                                             ::behavior
+                                             ::presents]))
+
+;; (s/explain-str ::wishlist-detailled
+;;                {:recipient "Florent" :behavior 8
+;;                 :presents ["Clojure book" "beers" "hat"]})
+
+(s/def ::present (s/keys :req-un [::name]))
+
