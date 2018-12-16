1f48cbfccdde8ac19372babb0081eb8ba318e00a
Flo <flal@melix.net>
2018-12-16 21:32:33 +0100

    hello world api

--- santa-api/project.clj
+++ santa-api/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [metosin/compojure-api "2.0.0-alpha28"]
                  [ring "1.7.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}
              :source-paths ["dev" "src"]})
--- santa-api/src/santa_api/core.clj
+++ santa-api/src/santa_api/core.clj
@@ -1,10 +1,15 @@
 (ns santa-api.core
   (:require [ring.util.response :as response]
+            [compojure.api.sweet :refer :all]
+            [compojure.route :as route]
             [ring.adapter.jetty :refer [run-jetty]]))
 
-(defn handler [rq]
-  (response/response "Merry Christmas !"))
-
+(def handler-api
+  (api
+   (GET "/merry" []
+     (response/response {:msg "Merry Christmas !"
+                         :year 2018}))
+   (route/not-found (response/not-found "Oups"))))
 
 (defn start-server []
-  (run-jetty #'handler {:port 8080 :join? false}))
+  (run-jetty #'handler-api {:port 8080 :join? false}))
